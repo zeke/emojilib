c79348e8a40bd85a9acfff2774843c3799925075
Zeke Sikelianos <zeke@sikelianos.com>
2016-06-19 12:45:52 -0700

    use stopwords; include emoji name in thesaurus search

--- extend.js
+++ extend.js
@@ -4,19 +4,22 @@ const log = console.error
 const thesaurus = require('thesaurus')
 const swearjar = require('swearjar')
 const emojis = require('./emojis.json')
-const commonWords = require('subtlex-word-frequencies')
-  .filter(w => w.count > 500)
+const stopwords = fs.readFileSync('./stopwords.txt', 'utf8').split('\n')
+const commonwords = require('subtlex-word-frequencies')
+  .filter(w => w.count > 50)
   .map(w => w.word.toLowerCase())
 
 Object.keys(emojis).forEach(key => {
   var emoji = emojis[key]
   log('\n\n', emoji.char)
-  emoji.keywords.forEach(keyword => {
+  var keywords = [key].concat(emoji.keywords)
+  keywords.forEach(keyword => {
     log('\n', keyword)
     thesaurus.find(keyword)
       .filter(word => {
         return emoji.keywords.indexOf(word) === -1 // no dupes
-        && commonWords.indexOf(word) > -1 // must be a common word
+        && stopwords.indexOf(word) === -1 // no stopwords
+        && commonwords.indexOf(word) > -1 // must be a common word
         && !swearjar.profane(word) // no profanity
       })
       .slice(0,5)
